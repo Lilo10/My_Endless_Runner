99978c7c9f46d9bd49fb41f1480f25eb5182868a
LailaNour <Laila.mohamedd310@gmail.com>
2025-12-15 16:33:48 +0200

    Collectible
    added coin collectible with texture and rotate script

---
+++ Assets/Scripts/CollectibleRotate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleRotate :MonoBehaviour
+{
+    [SerializeField] int rotateSpeed = 3
+;
+
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0, Space.World);
+    }
+}
