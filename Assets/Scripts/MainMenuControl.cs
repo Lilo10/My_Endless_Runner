b987a1836a60ac3d9149c21a8d06f12dfd71ac7c
LailaNour <Laila.mohamedd310@gmail.com>
2025-12-17 04:42:05 +0200

    Menu
    Added the main menu fade out animation and edited script

--- Assets/Scripts/MainMenuControl.cs
+++ Assets/Scripts/MainMenuControl.cs
@@ -5,9 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuControl : MonoBehaviour
 {
+    [SerializeField] GameObject FadeOut;
     public void StartGame()
     {
-        Time.timeScale = 1;
+        StartCoroutine(StartButton());
+    }
+
+    IEnumerator StartButton()
+    {
+        FadeOut.SetActive(true);
+        yield return new WaitForSeconds(1);
         SceneManager.LoadScene(1);
     }
 }
